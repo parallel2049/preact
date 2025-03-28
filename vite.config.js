e1815235cad4e6b3f57f2a51d765305960e9f04e
paralleluniverse <parallel2049@gmail.com>
2025-03-28 18:14:47 +0800

    main

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,15 @@ import preact from "@preact/preset-vite";
 export default defineConfig({
   plugins: [preact()],
   base: "/preact/",
+   build: {
+      target: 'esnext',  // 确保使用 ESNext 的构建方式
+      minify: false,
+    },
+    server: {
+      mimeTypes: {
+        jsx: 'application/javascript' // 确保 JSX 被识别为 JavaScript
+      },
+    },
 });
+
+
